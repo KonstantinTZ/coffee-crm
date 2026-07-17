05f90d861adfd59a74f2e73520895eaf95581223
Konstantin Tavaratsyan <147811423+KonstantinTZ@users.noreply.github.com>
2026-07-17 11:36:47 +0300

    add clock component

--- src/shared/OrderBoardPage/OrderBoardPage.jsx
+++ src/shared/OrderBoardPage/OrderBoardPage.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import './OrderBoardPage.css';
+import React from 'react'
+import './OrderBoardPage.css'
 import { InProcessBoard } from './InProcessBoard'
 import { PrepairedBoard } from './PrepairedBoard'
+import { Clock } from '../Clock'
 export function OrderBoardPage() {
   return (
     <div className="container">
+      <div className="row justify-content-end">
+        <div class="col-auto pe-0">
+          <Clock />
+        </div>
+      </div>
       <div className="row">
         <InProcessBoard />
         <PrepairedBoard />
       </div>
     </div>
-  );
+  )
 }
